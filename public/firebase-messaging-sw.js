9af5031d2316cab36c13597ac9f9b1397507270d
Wilfried KOFFI <glamolondon@gmail.com>
2021-04-24 19:53:19 +0000

    liste commande + notification FCM + config store et router

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -26,13 +26,4 @@ importScripts('https://www.gstatic.com/firebasejs/8.4.1/firebase-messaging.js');
  // https://firebase.google.com/docs/cloud-messaging/concept-options
  messaging.onBackgroundMessage(function(payload) {
    console.log('[firebase-messaging-sw.js] Received background message ', payload);
-   // Customize notification here
-   const notificationTitle = 'Background Message Title';
-   const notificationOptions = {
-     body: 'Background Message body.',
-     icon: '/firebase-logo.png'
-   };
- 
-   self.registration.showNotification(notificationTitle,
-     notificationOptions);
  });
