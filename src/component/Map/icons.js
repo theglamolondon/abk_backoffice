739e8baddb0dad8bd3bd229e1e1d5a320ddb21f1
Wilfried KOFFI <glamolondon@gmail.com>
2022-06-30 14:00:20 +0100

    fix marker

--- src/component/Map/icons.js
+++ src/component/Map/icons.js
@@ -1,15 +1,36 @@
 import L from 'leaflet';
 
-const iconResto = new L.Icon.Default({
-    imagePath: '/assets/images/map/resto-'
+const iconResto = new L.Icon({
+    iconUrl: '/assets/images/map/resto-marker-icon-2x.png',
+    iconRetinaUrl: '/assets/images/map/resto-marker-icon-2x.png',
+    iconAnchor: null,
+    shadowAnchor: null,
+    shadowRetinaUrl: null,
+    shadowSize: null,
+    iconSize: [35,35],
+    className: 'abk-map-icon'
 });
 
-const iconClient = new L.Icon.Default({
-    imagePath: '/assets/images/map/client-'
+const iconClient = new L.Icon({
+    iconUrl: '/assets/images/map/client-marker-icon-2x.png',
+    iconRetinaUrl: '/assets/images/map/client-marker-icon-2x.png',
+    iconAnchor: null,
+    shadowAnchor: null,
+    shadowRetinaUrl: null,
+    shadowSize: null,
+    iconSize: [35,35],
+    className: 'abk-map-icon'
 });
 
-const iconLivreur = new L.Icon.Default({
-    imagePath: '/assets/images/map/livreur-'
+const iconLivreur = new L.Icon({
+    iconUrl: '/assets/images/map/livreur-marker-icon-2x.png',
+    iconRetinaUrl: '/assets/images/map/livreur-marker-icon-2x.png',
+    iconAnchor: null,
+    shadowAnchor: null,
+    shadowRetinaUrl: null,
+    shadowSize: null,
+    iconSize: [35,35],
+    className: 'abk-map-icon'
 });
 
 const MapIcon = {
